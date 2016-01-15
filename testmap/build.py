0ffe5545a1f7f761dcd5276fef4d69177214d6cb
Bart Veldstra <bart.veldstra@gmail.com>
2016-01-15 20:54:48 +0100

    Compile all testmap maps into the asset bundle

--- testmap/build.py
+++ testmap/build.py
@@ -4,20 +4,24 @@ import os, subprocess, sys, shutil
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-mapname = "testmap"
-input_file = mapname + ".map"
-output_file = mapname + ".bsp"
-asset_dir = os.path.normpath(os.path.join(script_dir, "..", "assets"))
+def compile_map(mapname):
+	input_file = mapname + ".map"
+	output_file = mapname + ".bsp"
+	asset_dir = os.path.normpath(os.path.join(script_dir, "..", "assets"))
 
-qbsp_tool = os.path.join(script_dir, "qbsp")
-light_tool = os.path.join(script_dir, "light")
-vis_tool = os.path.join(script_dir, "vis")
+	qbsp_tool = os.path.join(script_dir, "qbsp")
+	light_tool = os.path.join(script_dir, "light")
+	vis_tool = os.path.join(script_dir, "vis")
 
 
-subprocess.check_call((qbsp_tool, input_file))
-subprocess.check_call((light_tool, input_file))
-subprocess.check_call((vis_tool, input_file))
+	subprocess.check_call((qbsp_tool, input_file))
+	subprocess.check_call((light_tool, input_file))
+	subprocess.check_call((vis_tool, input_file))
 
-src = os.path.join(script_dir, output_file)
-dst = os.path.join(asset_dir, output_file)
-shutil.copy(src, dst)
+	src = os.path.join(script_dir, output_file)
+	dst = os.path.join(asset_dir, output_file)
+	shutil.copy(src, dst)
+
+mapfiles = (f for f in os.listdir(script_dir) if f.endswith(".map"))
+for file_ in mapfiles:
+	compile_map(file_[:-4])
