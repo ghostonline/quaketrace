bf79a456f97dc5e53e139b1da1832bf2f9b1ddc2
Bart Veldstra <bart.veldstra@gmail.com>
2016-01-26 19:38:41 +0100

    Asset bundler creates arrays instead of strings

--- assets/bundle.py
+++ assets/bundle.py
@@ -2,7 +2,10 @@
 import os, textwrap, sys
 
 script_src = os.path.dirname(os.path.abspath(__file__))
-output = sys.argv[1]
+if len(sys.argv) < 2:
+	output = os.path.join(script_src, "bundled.h")
+else:
+	output = sys.argv[1]
 
 assets = []
 files = sorted(f for f in os.listdir(script_src) if f.endswith(".bmp") or f.endswith(".bsp"))
@@ -11,9 +14,9 @@ for file_ in files:
 	varname = "ASSET_%s" % os.path.splitext(file_)[0].upper()
 	with open(full_path, "rb") as f:
 		data = f.read().encode("hex").upper()
-	formatted ="".join(["\\x"+x+y for (x,y) in zip(data[0::2], data[1::2])])
-	formatted = "\" \\\n\t\"".join(textwrap.wrap(formatted,80))
-	array = 'static const char %s[] =\n\t"%s";' % (varname, formatted)
+	formatted ="".join(["0x"+x+y+", " for (x,y) in zip(data[0::2], data[1::2])])
+	formatted = "\n\t".join(textwrap.wrap(formatted,80))
+	array = 'static const unsigned char %s[] = {\n\t%s\n};' % (varname, formatted)
 	assets.append(array)
 
 with open(output, "w") as f:
