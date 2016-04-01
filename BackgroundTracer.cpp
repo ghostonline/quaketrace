c8c5cdcdb342ea29f2073f1d0442b99563681ab7
Bart Veldstra <bart.veldstra@gmail.com>
2016-04-01 07:34:08 +0200

    Run raytrace task in background

--- BackgroundTracer.cpp
+++ BackgroundTracer.cpp
@@ -1,11 +1,39 @@
 #include "BackgroundTracer.hpp"
+#include "Assert.hpp"
+#include "Scene.hpp"
 
 BackgroundTracer::BackgroundTracer(const RayTracer::Config& config)
-: engine(config)
+: running(false)
+, engine(config)
 , canvas(config.width, config.height, Image::FORMAT_ARGB)
-{}
+{
+}
+
+BackgroundTracer::~BackgroundTracer()
+{
+    running = false;
+    if (thread.joinable()) { thread.join(); }
+}
 
 void BackgroundTracer::startTrace(const Scene& scene)
 {
-    engine.trace(scene, &canvas);
+    ASSERT(this->scene == nullptr);
+    this->scene.reset(new Scene(scene));
+
+    if (!running)
+    {
+        running = true;
+        thread = std::thread(threadRunner, this);
+    }
+}
+
+void BackgroundTracer::traceAsync()
+{
+    std::this_thread::yield();
+    if (scene)
+    {
+        engine.trace(*scene, &canvas);
+        scene.reset();
+    }
+    running = false;
 }
--- BackgroundTracer.hpp
+++ BackgroundTracer.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 #include "RayTracer.hpp"
+#include <memory>
+#include <thread>
 
 class BackgroundTracer
 {
 public:
     BackgroundTracer(const RayTracer::Config& config);
+    ~BackgroundTracer();
 
     void startTrace(const Scene& scene);
     float getProgress() { return 0; }
     const Image& getCanvas() const { return canvas; }
 
 private:
+    static void threadRunner(BackgroundTracer* self) { self->traceAsync(); }
+    void traceAsync();
+
+    bool running;
     RayTracer engine;
+    std::unique_ptr<Scene> scene;
     Image canvas;
+
+    std::thread thread;
 };
