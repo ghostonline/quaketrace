d1522978817f399f9d3f69848a3795e6830e08c1
Bart Veldstra <bart.veldstra@gmail.com>
2016-04-06 17:57:06 +0200

    Select front end based on compile flag

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@
 #include "SDL.h" // Convert main into winmain if necessary
 
 int main(int argc, char* argv[]) {
+#if SHOW_GUI
+    return GUI::runUntilFinished(argc, argv);
+#else
     return Console::runUntilFinished(argc, argv);
+#endif
 }
